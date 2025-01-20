b83f8b5874343f7df17e2a0fc73f5377df13e357
seque21plw <juagomezse@unal.edu.co>
2025-01-20 11:47:34 -0500

    tercera version del codigo testi1.py

--- test1.py
+++ test1.py
@@ -1,23 +1,22 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore,db
 
 # Initialize the Firebase app
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate('testpython-673c0.json')
+firebase_admin.initialize_app(cred, {"databaseURL": "https://testpython-673c0.firebaseio.com"})
 
-
+ref = db.reference('users')
 
 # Initialize Firestore
 db = firestore.client()
 
-# Example function to add data to Firestore
+# Example function to add data to Firestore 
 def add_data(collection_name, document_name, data):
     db.collection(collection_name).document(document_name).set(data)
 
 # Example usage
-data = {
-    'name': 'John Doe',
-    'age': 30,
-    'email': 'johndoe@example.com'
+registro = {
+    'usuario': input ("ingrese su usuario: "),
+    'email': input('ingrese su email: ')
 }
-add_data('users', 'user1', data)
+add_data('users', 'user1', registro)
