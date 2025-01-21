4a6d143b3ae6c00f105df0b85d42d69a8f804f90
seque21plw <juagomezse@unal.edu.co>
2025-01-20 20:12:24 -0500

    commit generado luego de testeo en firebase

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore,db
 
 # Initialize the Firebase app
 cred = credentials.Certificate('testpython-673c0.json')
-firebase_admin.initialize_app(cred, {"databaseURL": "https://testpython-673c0.firebaseio.com"})
+firebase_admin.initialize_app(cred, {"databaseURL": "https://testpython-673c0-default-rtdb.firebaseio.com/"})
 
 ref = db.reference('users')
 
@@ -15,8 +15,10 @@ def add_data(collection_name, document_name, data):
     db.collection(collection_name).document(document_name).set(data)
 
 # Example usage
-registro = {
+ref.set({ 
     'usuario': input ("ingrese su usuario: "),
     'email': input('ingrese su email: ')
-}
-add_data('users', 'user1', registro)
+})
+
+def escuchar_eventos(event):
+    print(f"cambio: {event.document_id}")
